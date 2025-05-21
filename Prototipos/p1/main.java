82d7fb254f4cfa06902b9c0b323cc8b23fa01a00
Filipe Schneider Schirmer - 12924111558 <12924111558@ulife.com.br>
2025-05-21 11:28:59 -0300

    Feat: exercicios

--- Prototipos/p1/main.java
+++ Prototipos/p1/main.java
@@ -1,5 +1,15 @@
 package Prototipos.p1;
+import java.io.FileReader;
 
 public class main {
-    
+    public static void main(String[] args) {
+        try {
+            Lexer lexer = new Lexer(new FileReader(args[0]));
+            Parser parser = new Parser(lexer);
+            parser.parse();
+            System.out.println("Parsing completed successfully.");
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("An error occurred: " + e.getMessage());}
+    }
 }
